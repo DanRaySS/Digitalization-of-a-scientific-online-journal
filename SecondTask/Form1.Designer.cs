a7a0d33a7c2a75666b54366266cdff29996594f2
Alexey <107277378+PPnotGG@users.noreply.github.com>
2023-12-03 23:28:05 +0500

    Name constructor completed

--- SecondTask/Form1.Designer.cs
+++ SecondTask/Form1.Designer.cs
@@ -32,11 +32,20 @@
             label1 = new Label();
             textBox1 = new TextBox();
             groupBox1 = new GroupBox();
-            label2 = new Label();
+            label4 = new Label();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
             label3 = new Label();
-            comboBox1 = new ComboBox();
+            comboBox3 = new ComboBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
             comboBox2 = new ComboBox();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            numericUpDown1 = new NumericUpDown();
             groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -52,7 +61,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 398);
+            label1.Location = new Point(12, 408);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 1;
@@ -67,56 +76,143 @@
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(comboBox2);
-            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox3);
             groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(comboBox1);
             groupBox1.Controls.Add(label2);
             groupBox1.Location = new Point(12, 50);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(213, 100);
+            groupBox1.Size = new Size(231, 349);
             groupBox1.TabIndex = 3;
             groupBox1.TabStop = false;
-            groupBox1.Text = "Имя";
+            groupBox1.Text = "Авторы";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Enabled = false;
+            label4.Location = new Point(9, 255);
+            label4.Name = "label4";
+            label4.Size = new Size(93, 15);
+            label4.TabIndex = 18;
+            label4.Text = "Кол-во авторов";
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(9, 226);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(61, 19);
+            checkBox4.TabIndex = 16;
+            checkBox4.Text = "“et al.”";
+            checkBox4.UseVisualStyleBackColor = true;
+            checkBox4.Click += checkBox4_Click;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(9, 291);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(128, 49);
+            checkBox3.TabIndex = 15;
+            checkBox3.Text = "Союз “and” между\r\nпоследним и \r\nпредпоследним";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(9, 178);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 30);
+            label3.TabIndex = 14;
+            label3.Text = "Разделитель\r\nмежду авторами";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { ",", ";" });
+            comboBox3.Location = new Point(134, 182);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(86, 23);
+            comboBox3.TabIndex = 13;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(9, 87);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(108, 34);
+            checkBox2.TabIndex = 12;
+            checkBox2.Text = "Пробел между\r\nинициалами";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(9, 47);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(94, 34);
+            checkBox1.TabIndex = 11;
+            checkBox1.Text = "Точка после\r\nинициалов";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "\"\"", "\" \"", "\",\"", "\", \"" });
+            comboBox2.Location = new Point(134, 134);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(86, 23);
+            comboBox2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(9, 125);
+            label5.Name = "label5";
+            label5.Size = new Size(116, 45);
+            label5.TabIndex = 9;
+            label5.Text = "Разделитель\r\nмежду инициалами\r\nи фамилией";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Инициалы/Фамилия", "Фамилия/Инициалы" });
+            comboBox1.Location = new Point(87, 18);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(133, 23);
+            comboBox1.TabIndex = 5;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Location = new Point(9, 21);
             label2.Name = "label2";
-            label2.Size = new Size(50, 15);
+            label2.Size = new Size(72, 15);
             label2.TabIndex = 0;
-            label2.Text = "Формат";
+            label2.Text = "Положение";
             // 
-            // label3
+            // numericUpDown1
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(9, 63);
-            label3.Name = "label3";
-            label3.Size = new Size(75, 15);
-            label3.TabIndex = 4;
-            label3.Text = "Разделитель";
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(95, 18);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(86, 23);
-            comboBox1.TabIndex = 5;
-            // 
-            // comboBox2
-            // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(95, 60);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(86, 23);
-            comboBox2.TabIndex = 6;
+            numericUpDown1.Enabled = false;
+            numericUpDown1.Location = new Point(134, 251);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(86, 23);
+            numericUpDown1.TabIndex = 19;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 460);
             Controls.Add(groupBox1);
             Controls.Add(textBox1);
             Controls.Add(label1);
@@ -125,6 +221,7 @@
             Text = "Form1";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -135,9 +232,17 @@
         private Label label1;
         private TextBox textBox1;
         private GroupBox groupBox1;
-        private Label label3;
         private Label label2;
-        private ComboBox comboBox2;
         private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label5;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private Label label3;
+        private ComboBox comboBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+        private Label label4;
+        private NumericUpDown numericUpDown1;
     }
 }
--- SecondTask/Form1.cs
+++ SecondTask/Form1.cs
@@ -1,4 +1,6 @@
+using Microsoft.VisualBasic;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
@@ -12,48 +14,117 @@ namespace WinFormsApp1
         {
             InitializeComponent();
             handler = new RequestsHandler();
+            comboBox1.SelectedItem = comboBox1.Items[0];
+            comboBox2.SelectedItem = comboBox2.Items[0];
+            comboBox3.SelectedItem = comboBox3.Items[0];
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
             if (textBox1.Text == "")
-                label1.Text = "Error, empty field";
+            {
+                label1.Text = "Error, empty field.";
+                return;
+            }
+
+            Response1 res = await handler.GetMetadata(textBox1.Text);
+            if (res.status == "error")
+            {
+                label1.Text = "Error, wrong input.";
+                return;
+            }
+
+            string firstW, secondW, initialEnd, initialsSeparator;
+            string nameSeparator = comboBox2.Text.Trim('"');
+            string authorsSeparator = $"{comboBox3.Text} ";
+
+            if (checkBox1.Checked)
+                initialEnd = ".";
+            else
+                initialEnd = "";
+
+            if (checkBox2.Checked)
+                initialsSeparator = " ";
+            else
+                initialsSeparator = "";
+
+
+            var authors = res.message.author;
+            int authorsLength = authors.Length;
+            if (checkBox4.Checked)
+            {
+                authorsLength = (int)numericUpDown1.Value;
+            }
+            string[] authorsStr = new string[authorsLength];
+
+            if (comboBox1.Text == "Инициалы/Фамилия")
+            {
+                for (int i = 0; i < authorsLength; i++)
+                {
+                    matches = regex.Matches(authors[i].given);
+                    string[] initials = new string[matches.Count];
+                    for (int j = 0; j < matches.Count(); j++)
+                        initials[j] = $"{matches[j].Value}{initialEnd}";
+
+                    firstW = String.Join(initialsSeparator, initials);
+                    secondW = authors[i].family;
+
+                    authorsStr[i] =
+                        $"{firstW}{nameSeparator}{secondW}";
+                }
+            }
             else
             {
-                Response1 res = await handler.GetMetadata(textBox1.Text);
-
-                if (res.status == "error")
-                    label1.Text = "Error, wrong DOI";
-                else
+                for (int i = 0; i < authorsLength; i++)
                 {
-                    string doiText;
-                    var authors = res.message.author;
-                    string[] authorsStr = new string[authors.Length];
-                    string[] initials;
-                    string initialsSeparator = " ";
-                    string nameSeparator = " ";
-                    string authorSeparator = ", ";
+                    matches = regex.Matches(authors[i].given);
+                    string[] initials = new string[matches.Count];
+                    for (int j = 0; j < matches.Count(); j++)
+                        initials[j] = $"{matches[j].Value}{initialEnd}";
 
-                    for (int i = 0; i < authors.Length; i++) 
-                    {
-                        matches = regex.Matches(authors[i].given);
-                        initials = new string[matches.Count];
-                        for (int j = 0; j < matches.Count(); j++)
-                            initials[j] = $"{matches[j].Value}.";
+                    firstW = authors[i].family;
+                    secondW = String.Join(initialsSeparator, initials);
 
-                        authorsStr[i] = 
-                            $"{String.Join(initialsSeparator, initials)}{nameSeparator}{authors[i].family}";
-
-                        foreach (Match match in matches)
-                            Console.WriteLine(match.Value);
-                    }
-
-                    doiText = String.Join(authorSeparator, authorsStr);
-
-                    label1.Text = doiText;
+                    authorsStr[i] =
+                        $"{firstW}{nameSeparator}{secondW}";
                 }
             }
 
+            if (checkBox3.Checked && authorsLength >= 2)
+            {
+                string lastAuthor = authorsStr.Last();
+                List<string> tempAuthorsStr = new List<string>(authorsStr);
+                tempAuthorsStr.RemoveAt(authorsLength - 1);
+                authorsStr = tempAuthorsStr.ToArray();
+
+                label1.Text = $"{String.Join(authorsSeparator, authorsStr)} and {lastAuthor}";
+
+                return;
+            }
+
+            label1.Text = String.Join(authorsSeparator, authorsStr);
+
+            if (checkBox4.Checked)
+            {
+                label1.Text = $"{label1.Text} et al.";
+            }
+        }
+
+        private void checkBox4_Click(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                label4.Enabled = true;
+                numericUpDown1.Enabled = true;
+                checkBox3.Enabled = false;
+                checkBox3.Checked = false;
+            }
+            else
+            {
+                label4.Enabled = false;
+                numericUpDown1.Enabled = false;
+                checkBox3.Enabled = true;
+            }
         }
     }
 }
